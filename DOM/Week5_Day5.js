db493358e7acd21203cfd89a7008406f0612bdc7
fadlykayo <f_kayo@yahoo.com>
2016-12-01 16:31:22 +0700

    Adding new project

---
+++ DOM/Week5_Day5.js
@@ -0,0 +1,28 @@
+// Week 5, Day 5
+
+/*var body = document.body;
+
+var openingDiv = document.createElement("div");
+openingDiv.setAttribute("id", "opening");
+body.appendChild(openingDiv);
+
+var mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "main");
+body.appendChild(mainDiv);
+
+// main div
+
+var marketDiv = document.createElement("div");
+marketDiv.setAttribute("id", "market");
+mainDiv.appendChild(marketDiv);
+
+var eastGateDiv = document.createElement("div");
+eastGateDiv.setAttribute("id", "east-gate");
+mainDiv.appendChild(eastGateDiv);
+
+var eastForestDiv = document.createElement("div");
+eastForestDiv.setAttribute("id", "east-forest");
+mainDiv.appendChild(eastForestDiv);
+
+// opening div
+*/
