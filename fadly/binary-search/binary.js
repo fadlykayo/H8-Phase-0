3e583ea7ec57a80516ac8038b522f9d9445a63a3
fadlykayo <f_kayo@yahoo.com>
2017-01-08 17:55:09 +0700

    Revisi binary search

--- fadly/binary-search/binary.js
+++ fadly/binary-search/binary.js
@@ -20,19 +20,18 @@
 var test_array_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var test_array_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-function binary_search(search, array) {
-  var index = 0;
+function binary_search(search, array, index = 0) {
   index = Math.round(array.length / 2) - 1;
   if (search == array[index]) {
-    return array[index]-1;
+    return search-1;
   }
   else if (search < array[index]) {
-    array.splice(index, index+1);
-    return binary_search(search, array);
+    array = array.slice(0, index);
+    return binary_search(search, array, index);
   }
   else if (search > array[index]) {
-    array.splice(0, index+1);
-    return binary_search(search, array);
+    array = array.slice(index+1, array.length);
+    return binary_search(search, array, index);
   }
   else {
     return -1;
@@ -40,9 +39,9 @@ function binary_search(search, array) {
 }
 
 // Driver code
-console.log(binary_search(5, test_array_a)) //4
-console.log(binary_search(6, test_array_b)) //5
-console.log(binary_search(10, test_array_a)) //9
-console.log(binary_search(11, test_array_b)) //10
-console.log(binary_search(2, test_array_a)) //1
-console.log(binary_search(2, test_array_b)) //1
+console.log(binary_search(5, test_array_a))
+console.log(binary_search(6, test_array_b))
+console.log(binary_search(10, test_array_a))
+console.log(binary_search(11, test_array_b))
+console.log(binary_search(2, test_array_a))
+console.log(binary_search(2, test_array_b))
